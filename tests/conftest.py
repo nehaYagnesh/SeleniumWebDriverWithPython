bc68befa25c17d7f32f67b0bbce5286b9ecc18d7
nparekh <Neha.Parekh@soti.net>
2020-12-22 18:14:50 -0500

    Headless Chrome

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,42 @@
 """
 This module contains shared fixtures
 """
-
+import json
 import pytest
 import selenium.webdriver
 
 @pytest.fixture
-def browser():
+def config(scope='session'):
 
-    # Initialize the ChromeDriver instance
-    b = selenium.webdriver.Chrome("C:/Selenium/chromedriver.exe")
+    # Read the config file 
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+
+    # Assert if the values are acceptable
+    assert config['browser'] in ['Firefox','Chrome','Headless Chrome']
+    assert isinstance(config['implicit_wait'],int)
+    assert config['implicit_wait'] > 0
+
+    #Return config so it can be used
+    return config
+
+@pytest.fixture
+def browser(config):
+
+    #Initialize the WebDriver instance
+    if config['browser'] == 'Firefox':
+        b = selenium.webdriver.Firefox()
+    elif config['browser'] == 'Chrome':
+         b = selenium.webdriver.Chrome("C:/Selenium/chromedriver.exe")
+    elif config['browser'] == 'Headless Chrome':
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument("headless")
+        b = selenium.webdriver.Chrome('C:/Selenium/chromedriver.exe',options=opts)
+    else:
+        raise Exception(f'Browser "{config["browser"]}" is not supported')
 
     # Make its calls wait up to 10 seconds for elements to appear
-    b.implicitly_wait(10)
+    b.implicitly_wait(config['implicit_wait'])
 
     # Return to webdriver instance for the setup
     yield b
